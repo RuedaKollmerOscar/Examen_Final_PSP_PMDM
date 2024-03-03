065670ca2e69a7ed5a83e84f993e10b004fef991
Oscar <oscarwruedakollmer@gmail.com>
2024-03-03 19:43:21 +0100

    Se ha implementado el mapa con ubicaciones de ejemplo.

---
+++ lib/FirestoreObjects/FbTienda.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FbTienda {
+
+  final String name;
+  final String loc;
+  GeoPoint geoloc;
+
+  FbTienda ({
+    required this.name,
+    required this.loc,
+    required this.geoloc
+  });
+
+  factory FbTienda.fromFirestore(
+      DocumentSnapshot<Map<String, dynamic>> snapshot,
+      SnapshotOptions? options,
+      ) {
+    final data = snapshot.data();
+    return FbTienda(
+      name: data?['name'] != null ? data!['name'] : "xxxx",
+      loc: data?['loc'] != null ? data!['loc'] : "xxxx",
+      geoloc: data?['geoloc'] != null ? data!['geoloc'] : GeoPoint(0, 0)
+    );
+
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      if (name != null) "name" : name,
+      if (loc != null) "loc" : loc,
+      if (geoloc != null) "geoloc": geoloc,
+    };
+  }
+}
