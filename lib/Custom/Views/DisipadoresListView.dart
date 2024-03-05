c9fcee771c9cec39b4ba9e3d358044b58efd27d6
Oscar <oscarwruedakollmer@gmail.com>
2024-03-05 02:38:33 +0100

    Se ha implementado el catalogo de disipadores

---
+++ lib/Custom/Views/DisipadoresListView.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class DisipadoresListView extends StatelessWidget {
+  final String sNombre;
+  final String sColor;
+  final String sMaterial;
+  final int iVelocidadRotacionMinima;
+  final int iVelocidadRotacionMaxima;
+  final double dPrecio;
+  final String sUrlImg;
+  final int iPosicion;
+  final Function(int indice)? fOnItemTap;
+
+  const DisipadoresListView({
+    Key? key,
+    required this.sNombre,
+    required this.sColor,
+    required this.sMaterial,
+    required this.iVelocidadRotacionMinima,
+    required this.iVelocidadRotacionMaxima,
+    required this.dPrecio,
+    required this.sUrlImg,
+    required this.iPosicion,
+    this.fOnItemTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => fOnItemTap!(iPosicion),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Theme.of(context).colorScheme.primary,
+            width: 2,
+          ),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              sNombre,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 8),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Image.network(
+                sUrlImg,
+                width: 100,
+                height: 100,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Color: $sColor',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            Text(
+              'Material: $sMaterial',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            Text(
+              'Velocidad Rotación (Min-Max): $iVelocidadRotacionMinima - $iVelocidadRotacionMaxima RPM',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            Text(
+              'Precio: $dPrecio €',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/FirestoreObjects/FbDisipador.dart
@@ -0,0 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FbDisipador {
+  final String sNombre;
+  final String sColor;
+  final String sMaterial;
+  final int iVelocidadRotacionMinima;
+  final int iVelocidadRotacionMaxima;
+  final double dPrecio;
+  final String sUrlImg;
+
+  FbDisipador({
+    required this.sNombre,
+    required this.sColor,
+    required this.sMaterial,
+    required this.iVelocidadRotacionMinima,
+    required this.iVelocidadRotacionMaxima,
+    required this.dPrecio,
+    required this.sUrlImg,
+  });
+
+  factory FbDisipador.fromFirestore(
+      DocumentSnapshot<Map<String, dynamic>> snapshot,
+      SnapshotOptions? options,
+      ) {
+    final data = snapshot.data();
+    return FbDisipador(
+      sNombre: data?['nombre'] != null ? data!['nombre'] : "xxxx",
+      sColor: data?['color'] != null ? data!['color'] : "xxxx",
+      sMaterial: data?['material'] != null ? data!['material'] : "xxxx",
+      iVelocidadRotacionMinima: (data?['velocidadRotacionMinima'] as num?)?.toInt() ?? -1,
+      iVelocidadRotacionMaxima: (data?['velocidadRotacionMaxima'] as num?)?.toInt() ?? -1,
+      dPrecio: (data?['precio'] as num?)?.toDouble() ?? -1,
+      sUrlImg: data?['urlImg'] != null ? data!['urlImg'] : "xxxx",
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      if (sNombre != null) "nombre": sNombre,
+      if (sColor != null) "color": sColor,
+      if (sMaterial != null) "material": sMaterial,
+      if (iVelocidadRotacionMinima != null) "velocidadRotacionMinima": iVelocidadRotacionMinima,
+      if (iVelocidadRotacionMaxima != null) "velocidadRotacionMaxima": iVelocidadRotacionMaxima,
+      if (dPrecio != null) "precio": dPrecio,
+      if (sUrlImg != null) "urlImg": sUrlImg,
+    };
+  }
+}
